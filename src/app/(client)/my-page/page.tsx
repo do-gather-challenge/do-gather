5784e4e36ceb4c3090f6f47236a44a427067d1a5
ye21iin <kathykr120@gmail.com>
2025-03-21 23:04:17 +0900

    design:#5 my-page/프로필수정 UI

--- src/app/(client)/my-page/page.tsx
+++ src/app/(client)/my-page/page.tsx
@@ -5,18 +5,20 @@ import MyChallenge from './components/MyChallenge';
 
 const MyPage = () => {
   return (
-    <Tabs defaultValue="profile" className="mt-30 flex w-full items-center">
-      <TabsList>
-        <TabsTrigger value="profile">마이페이지</TabsTrigger>
-        <TabsTrigger value="challenge">마이챌린지</TabsTrigger>
-      </TabsList>
-      <TabsContent value="profile">
-        <EditProfile />
-      </TabsContent>
-      <TabsContent value="challenge">
-        <MyChallenge />
-      </TabsContent>
-    </Tabs>
+    <div className="flex h-screen items-center justify-center">
+      <Tabs defaultValue="profile" className="mt-10 flex w-full flex-col items-center">
+        <TabsList className="mx-auto">
+          <TabsTrigger value="profile">마이페이지</TabsTrigger>
+          <TabsTrigger value="challenge">마이챌린지</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile" className="flex w-full justify-center">
+          <EditProfile />
+        </TabsContent>
+        <TabsContent value="challenge" className="flex w-full justify-center">
+          <MyChallenge />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 };
 
