3e9483be1f70f1e2043650a9b24dad87ab2e4c4a
ye21iin <kathykr120@gmail.com>
2025-03-21 21:40:12 +0900

    add:#5 shadcn/ui Button, Label 추가

--- src/app/(client)/my-page/components/EditProfile.tsx
+++ src/app/(client)/my-page/components/EditProfile.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import React from 'react';
 import DEFAULT_IMAGE from '/public/images/default_profile.png';
+import { Input } from '@/components/ui/input';
 
 const EditProfile = () => {
   return (
     <div className="flex flex-col items-center gap-10">
       <div className="flex flex-col items-center gap-5">
         <Image src={DEFAULT_IMAGE} alt="profile" width={200} height={200} className="m-5 mt-10 rounded-full" />
-        <div>input 위치</div>
+        <Input placeholder={`현재 닉네임: ${DEFAULT_NICKNAME}`} />
         <div>button 위치</div>
       </div>
       <div>챌린지 현황 COUNT 노출 위치</div>
@@ -16,3 +17,5 @@ const EditProfile = () => {
 };
 
 export default EditProfile;
+
+const DEFAULT_NICKNAME = '초기값';
