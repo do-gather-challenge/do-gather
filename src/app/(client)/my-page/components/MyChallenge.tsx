9cc300eba435d2079188057dab7ef6afca198329
ye21iin <kathykr120@gmail.com>
2025-03-21 21:32:43 +0900

    design:#5 프로필 이미지 UI

---
+++ src/app/(client)/my-page/components/MyChallenge.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyChallenge = () => {
+  return <div>MyChallenge</div>;
+};
+
+export default MyChallenge;
--- src/app/(client)/my-page/page.tsx
+++ src/app/(client)/my-page/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import EditProfile from './components/EditProfile';
+import MyChallenge from './components/MyChallenge';
 
 const MyPage = () => {
   return (
@@ -8,8 +10,12 @@ const MyPage = () => {
         <TabsTrigger value="profile">마이페이지</TabsTrigger>
         <TabsTrigger value="challenge">마이챌린지</TabsTrigger>
       </TabsList>
-      <TabsContent value="profile">마이페이지 내용</TabsContent>
-      <TabsContent value="challenge">마이챌린지 내용</TabsContent>
+      <TabsContent value="profile">
+        <EditProfile />
+      </TabsContent>
+      <TabsContent value="challenge">
+        <MyChallenge />
+      </TabsContent>
     </Tabs>
   );
 };
