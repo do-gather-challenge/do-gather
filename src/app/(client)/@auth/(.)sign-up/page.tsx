82bf1e93ba3cd0e3dcbdd7c0b95736bb36e54a50
woohyuckk <woohyuckk@gmail.com>
2025-03-22 18:01:28 +0900

    design:#9 sign-up 반응형 1차 구현

--- src/app/(client)/@auth/(.)sign-up/page.tsx
+++ src/app/(client)/@auth/(.)sign-up/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import SignUpForm from '@/components/features/auth/auth-sign-up-form';
+import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
 const SignUpPageModal = () => {
@@ -11,10 +12,16 @@ const SignUpPageModal = () => {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-sm">
-      <div className="flex flex-col items-center justify-center rounded-lg border-2 bg-white p-4 shadow-lg">
-        <h1>SignUpPageModal 가로 채기 성공</h1>
-        <button onClick={handleCloseModal}>닫기</button>
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs">
+      <div className="relative mx-10 mt-30 flex min-h-[380px] w-full min-w-[260px] flex-col items-center justify-evenly rounded-lg border-2 bg-white p-4 shadow-lg md:h-[575px] md:w-[335px]">
+        <Button
+          type="button"
+          className="absolute top-5 right-5 shadow-none bg-transparent hover:none text-black"
+          onClick={handleCloseModal}
+        >
+          X
+        </Button>
+        <h1 className="h-24 text-center align-middle text-6xl md:text-9xl">logo</h1>
         <SignUpForm />
       </div>
     </div>
