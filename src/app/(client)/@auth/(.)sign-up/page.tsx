21dc7fbc1a3233cfdf47c820c6c07c8f883b51a0
woohyuckk <woohyuckk@gmail.com>
2025-03-22 13:57:48 +0900

    feat:#9 sign-up-form parallel/intercept 페이지 추가

--- src/app/(client)/@auth/(.)sign-up/page.tsx
+++ src/app/(client)/@auth/(.)sign-up/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import SignUpForm from '@/components/features/auth/auth-sign-up-form';
+import { useRouter } from 'next/navigation';
+
 const SignUpPageModal = () => {
-  return <div>SignUpPageModal</div>;
+  const router = useRouter();
+
+  const handleCloseModal = () => {
+    router.back();
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-sm">
+      <div className="flex flex-col items-center justify-center rounded-lg border-2 bg-white p-4 shadow-lg">
+        <h1>SignUpPageModal 가로 채기 성공</h1>
+        <button onClick={handleCloseModal}>닫기</button>
+        <SignUpForm />
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPageModal;
--- src/components/features/auth/auth-sign-up-form.tsx
+++ src/components/features/auth/auth-sign-up-form.tsx
@@ -1,5 +1,91 @@
+import { Button } from '@/components/ui/button';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  email: z.string(),
+  nickname: z.string(),
+  password: z.string(),
+  confirmPassword: z.string()
+});
+
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-bold">이메일</FormLabel>
+              <FormControl>
+                <Input placeholder="example@email.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="nickname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-bold">닉네임</FormLabel>
+              <FormControl>
+                <Input placeholder="닉네임(2~12자)" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-bold">비밀번호</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="비밀번호(6~16자)" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-bold">비밀번호 확인</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="비밀번호를 한번 더 입력해주세요." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignUpForm;
