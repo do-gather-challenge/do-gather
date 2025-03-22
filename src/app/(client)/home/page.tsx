ea12d3bceac9d733fd2b89627b1b188675b5f2bc
woohyuckk <woohyuckk@gmail.com>
2025-03-23 00:18:08 +0900

    refact:#9 Home 원상태 복원

--- src/app/(client)/home/page.tsx
+++ src/app/(client)/home/page.tsx
@@ -1,21 +1,5 @@
-import Link from 'next/link';
-
-
-/**
- * 로그인 test를 하기 위해 임시로 Link를 생성하였습니다.
- * 
- */
 const HomePage = () => {
-  return (
-    <div className='mt-24'>
-      HomePage
-      <Link href="/sign-in" className="border-2 border-amber-700 p-4">
-        로그인하러가기
-      </Link>
-      <Link href="/sign-up" className="border-2 border-amber-700 p-4">
-        회원가입하러가기
-      </Link>
-    </div>
-  );
+  return <div>HomePage</div>;
 };
+
 export default HomePage;
