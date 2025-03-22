83f256fa4c57b237927f0f441687c35fdba09ffc
woohyuckk <woohyuckk@gmail.com>
2025-03-22 14:34:23 +0900

    test:#9 Home에서 회원가입/로그인 버튼 임시작성

--- src/app/(client)/home/page.tsx
+++ src/app/(client)/home/page.tsx
@@ -1,4 +1,16 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className='mt-24'>
+      HomePage
+      <Link href="/sign-in" className="border-2 border-amber-700 p-4">
+        로그인하러가기
+      </Link>
+      <Link href="/sign-up" className="border-2 border-amber-700 p-4">
+        회원가입하러가기
+      </Link>
+    </div>
+  );
 };
 export default HomePage;
