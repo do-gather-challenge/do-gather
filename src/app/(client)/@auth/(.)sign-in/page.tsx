592630b2a359c3e1f8d8d0ccbfd3686c761631bb
woohyuckk <woohyuckk@gmail.com>
2025-03-22 13:57:37 +0900

    feat:#9 sign-in-form parallel/intercept 페이지 추가

--- src/app/(client)/@auth/(.)sign-in/page.tsx
+++ src/app/(client)/@auth/(.)sign-in/page.tsx
@@ -1,5 +1,23 @@
+'use client';
+import SignInForm from '@/components/features/auth/auth-sign-in-form';
+import { useRouter } from 'next/navigation';
+
 const SignInPageModal = () => {
-  return <div>SignInPageModal</div>;
+  const router = useRouter();
+
+  const handleCloseModal = () => {
+    router.back();
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-sm">
+      <div className="flex flex-col items-center justify-center rounded-lg border-2 bg-white p-4 shadow-lg">
+        <h1> SignInPageModal 가로 채기 성공</h1>
+        <SignInForm />
+        <button onClick={handleCloseModal}>닫기</button>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPageModal;
--- src/components/features/auth/auth-sign-in-form.tsx
+++ src/components/features/auth/auth-sign-in-form.tsx
@@ -1,5 +1,69 @@
+import { Button } from '@/components/ui/button';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, useForm } from 'react-hook-form';
+
+('use client');
+
+import { z } from 'zod';
+
+const formSchema = z.object({
+  email: z.string(),
+  password: z.string()
+});
+
 const SignInForm = () => {
-  return <div>SignInForm</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit = () => {};
+
+  const signInWithGithub = () => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="비밀번호를 입력해주세요" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+        <Button type="button" onClick={signInWithGithub}>
+          Sign in With GitHub
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignInForm;
