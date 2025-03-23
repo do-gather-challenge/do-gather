dd36ca857627abed2a337bfa720298415ac8772d
llddang <bodyness@naver.com>
2025-03-23 22:54:59 +0900

    fix:#12 빌드 에러 수정

--- src/components/features/challenges/post/challenge-post-button-group.tsx
+++ src/components/features/challenges/post/challenge-post-button-group.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { ChallengePost } from '@/lib/hooks/use-challenge-form';
+import { ChallengePost } from '@/types/challenge.type';
 
 type ChallengePostButtonGroupProps = {
   challenge: ChallengePost;
