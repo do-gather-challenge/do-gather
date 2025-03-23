be51486596a8a8ec70c0a18ec271c45e608fb2fb
woohyuckk <woohyuckk@gmail.com>
2025-03-23 17:51:18 +0900

    feat:#20 use-sign-in-form 수정

--- src/lib/hooks/use-sign-in-form.ts
+++ src/lib/hooks/use-sign-in-form.ts
@@ -1,8 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import browserClient from '../supabase/client';
+import { useRouter } from 'next/navigation';
 const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$&*?!%])[A-Za-z\d!@$%&*?]{6,16}$/;
 export const useSignInForm = () => {
+  const router = useRouter();
+
   const signFormSchema = z.object({
     email: z.string().email({
       message: '유효한 이메일 형식이 아닙니다.'
@@ -29,8 +33,20 @@ export const useSignInForm = () => {
     }
   });
 
-  const onSubmit = (values: z.infer<typeof signFormSchema>) => {
-    console.log('Form submitted => ', values);
+  const onSubmit = async (values: z.infer<typeof signFormSchema>) => {
+    const { email, password } = values;
+    const { data, error } = await browserClient.auth.signInWithPassword({
+      email,
+      password
+    });
+
+    if (error) {
+      console.error(error.message);
+      alert('로그인 실패');
+    }
+    if (!!data.user) {
+      return router.back();
+    }
   };
 
   return { form, onSubmit };
