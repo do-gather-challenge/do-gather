b8f422281bae7b795669687bc5a3601c2eeccc20
woohyuckk <woohyuckk@gmail.com>
2025-03-23 16:25:54 +0900

    feat:#9 server.ts async 제거

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,8 +2,8 @@ import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@/constants/env.constant';
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export async function createClient() {
-  const cookieStore = await cookies();
+export function createClient() {
+  const cookieStore = cookies();
 
   return createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     cookies: {
