a66e9c7dcc32ce250da7b252621a4632c2df27af
woohyuckk <woohyuckk@gmail.com>
2025-03-23 23:08:05 +0900

    feat:#20 sign-up-input-field 배열 작성

--- src/components/features/auth/auth-sign-up-form.tsx
+++ src/components/features/auth/auth-sign-up-form.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
-import AuthInputForm from './auth-input-field';
+import AuthInputField from './auth-input-field';
 import { useSignUpForm } from '@/lib/hooks/use-sign-up-form';
 
 const AuthSignUpForm = () => {
@@ -11,22 +11,17 @@ const AuthSignUpForm = () => {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-4">
-        <AuthInputForm name="email" label="이메일" placeholder="mail@example.com" control={form.control} type="email" />
-        <AuthInputForm name="nickname" label="닉네임" placeholder="닉네임(2~12자)" control={form.control} type="text" />
-        <AuthInputForm
-          name="password"
-          label="비밀번호"
-          placeholder="비밀번호(6~12자)"
-          control={form.control}
-          type="password"
-        />
-        <AuthInputForm
-          name="confirmPassword"
-          label="비밀번호 확인"
-          placeholder="입력하신 비밀번호(6~12자)"
-          control={form.control}
-          type="password"
-        />
+        {SignUpInputField.map((input) => {
+          return (
+            <AuthInputField
+              control={form.control}
+              label={input.label}
+              name={input.name}
+              type={input.type}
+              placeholder={input.placeholder}
+            />
+          );
+        })}
 
         <Button type="submit" className="bg-secondary w-full text-white">
           회원가입 완료
@@ -37,3 +32,30 @@ const AuthSignUpForm = () => {
 };
 
 export default AuthSignUpForm;
+
+const SignUpInputField = [
+  {
+    name: 'email',
+    label: '이메일',
+    placeholder: 'mail@example.com',
+    type: 'email'
+  },
+  {
+    name: 'nickname',
+    label: '닉네임',
+    placeholder: '닉네임(2~12자)',
+    type: 'text'
+  },
+  {
+    name: 'password',
+    label: '비밀번호',
+    placeholder: '비밀번호(6~12자)',
+    type: 'password'
+  },
+  {
+    name: 'confirmPassword',
+    label: '비밀번호 확인',
+    placeholder: '입력하신 비밀번호(6~12자)',
+    type: 'password'
+  }
+] as const;
