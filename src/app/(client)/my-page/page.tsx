6c5e0b9026b03a4e9e10fa8f6b8b3996e1bf61c9
ye21iin <kathykr120@gmail.com>
2025-03-24 10:11:03 +0900

    design:#24 top margin 요소 수정

--- src/app/(client)/my-page/page.tsx
+++ src/app/(client)/my-page/page.tsx
@@ -6,7 +6,7 @@ import MyPageMyChallenge from '../../../components/my-page/my-page-my-challenge'
 const MyPage = () => {
   return (
     <section className="flex items-center justify-center">
-      <Tabs defaultValue="profile" className="mt-30 flex w-full flex-col items-center">
+      <Tabs defaultValue="profile" className="mt-10 flex w-full flex-col items-center">
         <TabsList className="mx-auto">
           <TabsTrigger value="profile">마이페이지</TabsTrigger>
           <TabsTrigger value="challenge">마이챌린지</TabsTrigger>
