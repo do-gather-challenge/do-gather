bc02835c03ca56087c7ef2fe25183e6f66fb5c26
ye21iin <kathykr120@gmail.com>
2025-03-24 19:43:52 +0900

    feat:#24 마이챌린지 버튼 리팩토링 및 상태 추가

--- src/components/my-page/my-page-my-challenge.tsx
+++ src/components/my-page/my-page-my-challenge.tsx
@@ -1,9 +1,25 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import DEFAULT_IMAGE from '/public/images/default_profile.png';
 import { Button } from '@/components/ui/button';
 
+type MyChallengeAlias = 'Today' | '참여중' | '완료';
+type MyPageMyChallenges = {
+  label: string;
+  alias: MyChallengeAlias;
+};
+
 const MyPageMyChallenge = () => {
+  const [selectedChallenge, setSelectedChallenge] = useState<MyChallengeAlias>('Today');
+
+  const challenges: MyPageMyChallenges[] = [
+    { label: '오늘 인증한 챌린지', alias: 'Today' },
+    { label: '참여 중인 챌린지', alias: '참여중' },
+    { label: '완료한 챌린지', alias: '완료' }
+  ];
+
   return (
     <section className="mt-5 flex w-full flex-col items-center gap-3 sm:flex-row">
       <div className="flex w-full flex-col items-center sm:w-[30%]">
@@ -19,21 +35,20 @@ const MyPageMyChallenge = () => {
           <p className="font-bold">Nickname</p>
         </div>
         <div className="flex gap-2 sm:mt-5 sm:flex-col">
-          <Button variant="primary" className="h-8 w-20 justify-center p-0 sm:h-12 sm:w-60 sm:justify-between sm:p-5">
-            <p className="sm:hidden">Today</p>
-            <p className="hidden sm:block">오늘 인증한 챌린지</p>
-          </Button>
-          <Button variant="primary" className="h-8 w-20 justify-center p-0 sm:h-12 sm:w-60 sm:justify-between sm:p-5">
-            <p className="sm:hidden">참여중</p>
-            <p className="hidden sm:block">참여 중인 챌린지</p>
-          </Button>
-          <Button variant="primary" className="h-8 w-20 justify-center p-0 sm:h-12 sm:w-60 sm:justify-between sm:p-5">
-            <p className="sm:hidden">완료</p>
-            <p className="hidden sm:block">완료한 챌린지</p>
-          </Button>
+          {challenges.map((challenge) => (
+            <Button
+              key={challenge.alias}
+              variant="primary"
+              className="h-8 w-20 justify-center p-0 sm:h-12 sm:w-60 sm:justify-between sm:p-5"
+              onClick={() => setSelectedChallenge(challenge.alias)}
+            >
+              <p className="sm:hidden">{challenge.alias}</p>
+              <p className="hidden sm:block">{challenge.label}</p>
+            </Button>
+          ))}
         </div>
       </div>
-      <div className="grid w-full grid-cols-1 p-5 sm:w-[70%] sm:grid-cols-3">페이지네이션 위치</div>
+      <div className="grid w-full grid-cols-1 p-5 sm:w-[70%] sm:grid-cols-3">{selectedChallenge}</div>
     </section>
   );
 };
