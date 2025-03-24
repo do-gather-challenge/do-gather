a6657789feab2429c00d0014c6b1c6341f08bcaa
ye21iin <kathykr120@gmail.com>
2025-03-25 01:38:51 +0900

    feat:#24 마이챌린지/완료된 챌린지 데이터 ChallengeCard 렌더링 in progress - 데이터 1개에 대해 구현 완료 - 동적 렌더링으로 확장 필요

--- src/components/my-page/my-page-my-completed-challenges.tsx
+++ src/components/my-page/my-page-my-completed-challenges.tsx
@@ -3,6 +3,7 @@
 import { useGetMyCompletedChallengesQuery } from '@/lib/queries/useGetMyCompletedChallengesQuery';
 import browserClient from '@/lib/supabase/client';
 import { useEffect, useState } from 'react';
+import ChallengeCard from '../features/challenges/challenge-card';
 
 const MyPageMyCompletedChallenges = () => {
   const [userId, setUserId] = useState<string | null>(null);
@@ -30,9 +31,24 @@ const MyPageMyCompletedChallenges = () => {
     return <>Error occurred... {error?.message}</>;
   }
 
-  console.log(challenges);
+  // console.log(challenges);
 
-  return <div>{!pageCount ? <p>완료된 챌린지가 없습니다.</p> : <></>}</div>;
+  return (
+    <div>
+      {!pageCount ? (
+        <p>완료된 챌린지가 없습니다.</p>
+      ) : (
+        <ChallengeCard
+          thumbnail={challenges[0].challengeImage}
+          category={challenges[0].category}
+          participants={challenges[0].participantCount}
+          title={challenges[0].title}
+          startDate={challenges[0].startDate}
+          endDate={challenges[0].finishDate}
+        ></ChallengeCard>
+      )}
+    </div>
+  );
 };
 
 export default MyPageMyCompletedChallenges;
