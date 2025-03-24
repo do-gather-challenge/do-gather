3619263d0326286dd271a996d9b98d8541175f7c
Jieun Lee <2ur.leeje@gmail.com>
2025-03-24 17:59:22 +0900

    fix:#14 supabase.co 호스트 네임 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sleczgiwuppubjccavne.supabase.co",
+        port: "",
+        pathname: "/**"
+      }
+    ],
+    formats: ["image/avif", "image/webp"]
+  }
 };
 
 module.exports = nextConfig;
