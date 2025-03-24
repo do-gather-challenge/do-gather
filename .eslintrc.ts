ff6e8d48d485b3bb09783086be1b097c1e096d22
Jieun Lee <2ur.leeje@gmail.com>
2025-03-24 21:09:24 +0900

    chore: prettier-format 전체 반영

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -2,33 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    jest: true,
+    jest: true
   },
   extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "prettier",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
   ],
-  plugins: [
-    "jest",
-    "prettier",
-    "@typescript-eslint",
-    "react",
-    "react-hooks",
-    "jsx-a11y",
-    "import",
-  ],
+  plugins: ['jest', 'prettier', '@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import'],
   rules: {
-    "prettier/prettier": ["error"],
-    "no-param-reassign": ["error", { props: false }],
-  },
+    'prettier/prettier': ['error'],
+    'no-param-reassign': ['error', { props: false }]
+  }
 };
--- src/components/ui/rounded-image.tsx
+++ src/components/ui/rounded-image.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './avatar';
 
 type RoundedImageProps = {
-  className: string,
-  src: string,
-  alt: string,
-  fallback?: string,
-  fallbackClassName?: string,
-}
-const RoundedImage = ( {className, src, alt, fallback='No Image', fallbackClassName}: RoundedImageProps) => {
+  className: string;
+  src: string;
+  alt: string;
+  fallback?: string;
+  fallbackClassName?: string;
+};
+const RoundedImage = ({ className, src, alt, fallback = 'No Image', fallbackClassName }: RoundedImageProps) => {
   return (
     <Avatar className={className}>
       <AvatarImage src={src} alt={alt} className="object-cover" />
