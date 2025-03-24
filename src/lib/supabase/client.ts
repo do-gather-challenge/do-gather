b45a986d2870c416527b0966fe48ba08eaf62dc3
verdantgreeny <verdantgreeny@gmail.com>
2025-03-24 18:25:06 +0900

    refactor: #17 상수 사용하도록 변경

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,10 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@/constants/env.constant';
+import { createBrowserClient } from '@supabase/ssr';
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+export const createClient = () => createBrowserClient(SUPABASE_URL!, SUPABASE_ANON_KEY!);
 
 const browserClient = createClient();
 
