49c4d54fc1f4364955f098a80cc4a8437cc53999
ye21iin <kathykr120@gmail.com>
2025-03-25 16:26:13 +0900

    fix:#24 merge시 삭제한 파일 복구

---
+++ src/lib/queries/useGetMyInProgressChallengesQuery.ts
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchGetMyInProgressChallengesByPage } from '../api/my-challenge.api';
+import { queryKeys } from '@/constants/query-keys';
+
+export const useGetMyInProgressChallengesQuery = (pageIndex: number, cardsPerPage: number) => {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: [queryKeys.MY_IN_PROGRESS_CHALLENGE, pageIndex, cardsPerPage],
+    queryFn: () => fetchGetMyInProgressChallengesByPage(pageIndex + 1, cardsPerPage)
+  });
+
+  const pageCount = data?.pagination.pageCount ?? 0;
+  const challenges = data?.data ?? [];
+
+  return { pageCount, challenges, isLoading, isFetching };
+};
