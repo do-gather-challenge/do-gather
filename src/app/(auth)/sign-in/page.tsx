26524e7503ea19a41e575ef41edc7192deae4d2e
woohyuckk <woohyuckk@gmail.com>
2025-03-25 09:35:46 +0900

    chore:#27 불필요한 주석제거

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -7,7 +7,6 @@ const SignInPage = () => {
     <section className="bg-primary-foreground flex min-h-screen w-full items-center justify-center">
       <figure className="m-8 flex w-full max-w-4xl flex-col items-center justify-center rounded-2xl bg-white shadow-2xl md:m-8 md:flex-row">
         <Link href="/home" className="flex w-full items-center justify-center md:w-1/2">
-          {/* 이미지 컨테이너에 명확한 높이 지정 */}
           <div className="relative flex h-64 w-3/4 items-center justify-center p-4 sm:h-72 md:h-96 md:w-full md:p-0">
             <Image
               src="/images/logo.png"
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -7,7 +7,6 @@ const SignUpPage = () => {
     <section className="bg-primary-foreground flex min-h-screen w-full items-center justify-center">
       <figure className="m-8 flex w-full max-w-4xl flex-col items-center justify-center rounded-2xl bg-white shadow-2xl md:m-8 md:flex-row">
         <Link href="/home" className="flex w-full items-center justify-center md:w-1/2">
-          {/* 이미지 컨테이너에 명확한 높이 지정 */}
           <div className="relative flex h-64 w-full items-center justify-center p-8 sm:h-72 md:h-96">
             <Image
               src="/images/logo.png"
