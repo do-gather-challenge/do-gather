3548fa79d807e9de1b54d5cf7a15f214284705c4
llddang <bodyness@naver.com>
2025-03-25 12:25:17 +0900

    fix:#25 build 에러 수정

--- src/components/features/challenges/home/challenge-home-participation-list.tsx
+++ src/components/features/challenges/home/challenge-home-participation-list.tsx
@@ -16,7 +16,7 @@ const ChallengeHomeParticipationList = ({ cardsPerPage, challenges, isPending }:
     <div className="relative w-full overflow-hidden">
       <div className={`grid place-items-center gap-6 ${getGridCols(cardsPerPage)}`}>
         {isPending
-          ? Array.from({ length: cardsPerPage }).map((_, i) => <ChallengeCardSkeleton key={i} />
+          ? Array.from({ length: cardsPerPage }).map((_, i) => <ChallengeCardSkeleton key={i} />)
           : challenges.map((challenge) => (
               <ChallengeCard
                 key={challenge.id}
