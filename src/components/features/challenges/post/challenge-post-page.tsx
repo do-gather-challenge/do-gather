17c440424bea9e0f3eb05c0cf7ddee52d2acb82a
verdantgreeny <verdantgreeny@gmail.com>
2025-03-25 19:18:09 +0900

    fix: #33 챌린지 생성 성공 시 홈으로 이동

---
+++ src/components/features/challenges/post/challenge-post-page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import ChallengePostSelector from '@/components/features/challenges/post/challenge-post-selector';
+import ChallengePostImageUploader from '@/components/features/challenges/post/challenge-post-image-uploader';
+import ChallengePostInput from '@/components/features/challenges/post/challenge-post-input';
+import ChallengePostButtonGroup from '@/components/features/challenges/post/challenge-post-button-group';
+import { useChallengeForm } from '@/lib/hooks/use-challenge-form';
+
+const ChallengePostPage: React.FC = () => {
+  const { challenge, challengeImageFile, setters, handleChange } = useChallengeForm();
+
+  return (
+    <section className="mx-auto mb-6 max-w-[320px] p-6 md:max-w-[640px]">
+      <h1 className="mb-6 text-2xl font-bold">챌린지 생성</h1>
+      <ChallengePostInput challenge={challenge} handleChange={handleChange} />
+
+      <section className="mb-6 grid grid-cols-1 gap-6 md:grid-cols-2">
+        <ChallengePostSelector challenge={challenge} setters={setters} />
+        <ChallengePostImageUploader setters={setters} challenge={challenge} />
+      </section>
+
+      <div className="flex justify-center gap-6">
+        <ChallengePostButtonGroup challenge={challenge} challengeImageFile={challengeImageFile} isEditMode={false} />
+      </div>
+    </section>
+  );
+};
+
+export default ChallengePostPage;
