35268eec811d8f8c5ce1589726278dc4f7e0b0c6
Jieun Lee <2ur.leeje@gmail.com>
2025-03-25 19:58:56 +0900

    feat:#37 헤더 UI 및 기능 구현

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,28 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '../ui/button';
-import { Avatar, AvatarImage } from '../ui/avatar';
-import { getUserInfo } from '@/lib/api/user-Info.api';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { getUserInfo, signOut } from '@/lib/api/user-Info.api';
 import LOGO_HORIZONTAL from '../../../public/images/logo-horizontal.png';
+import DEFAULT_PROFILE_IMAGE from '../../../public/images/default_profile.png';
 
 const Header = async () => {
   const { isLogin, userInfo } = await getUserInfo();
-  console.log(userInfo);
+
   return (
-    //헤더부분에 z-10 만으로도 shadcn 캐러셀 위로 배치 가능
-    <header className="bg-primary fixed top-0 z-10 flex h-15 w-full items-center justify-around md:h-20">
-      <Link href="/">
-        <Image src={LOGO_HORIZONTAL} alt="헤더 로고 이미지" height={50} width={100} />
+    //헤더 부분에 z-10 만으로도 shadcn 캐러셀 위로 배치 가능
+    <header className="bg-primary fixed top-0 z-10 flex h-15 w-full items-center justify-between px-20 md:h-20">
+      <Link href="/home">
+        <Image src={LOGO_HORIZONTAL} alt="헤더 로고 이미지" className="ml-3 h-auto w-[55%] object-contain" />
       </Link>
 
       <section className="flex items-center justify-center">
-        {`로그인 상태 isLogin 값 ==> ${isLogin}`}
         {isLogin ? (
-          <div className="flex gap-3">
-            <Button variant={'secondary'}>로그아웃</Button>
-          </div>
+          <form action={signOut} className="flex gap-3">
+            <Button type="submit" variant={'secondary'}>
+              로그아웃
+            </Button>
+          </form>
         ) : (
-          <div>
+          <div className="flex gap-3">
             <Link href="/sign-in">
               <Button variant={'secondary'}>로그인</Button>
             </Link>
@@ -33,11 +35,15 @@ const Header = async () => {
         )}
         {/* shadcn avatar */}
         {isLogin && (
-          <figure className="ml-3">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt={userInfo.nickname} />
+          <Link href="/my-page">
+            <Avatar className="mr-3 ml-3">
+              <AvatarImage
+                src={userInfo.profile_image || DEFAULT_PROFILE_IMAGE.src}
+                alt={userInfo.nickname || '게스트'}
+              />
+              <AvatarFallback>{userInfo.nickname.slice(0, 2)}</AvatarFallback>
             </Avatar>
-          </figure>
+          </Link>
         )}
       </section>
     </header>
