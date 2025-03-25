00bb351f6245d25923eabdbad23f6f59b4e877be
ye21iin <kathykr120@gmail.com>
2025-03-25 23:47:04 +0900

    feat:#24 마이챌린지 사용자 정보(프로필 이미지, 닉네임) 표시 기능 추가 - 서버액션 적용

--- src/components/my-page/my-page-my-challenge.tsx
+++ src/components/my-page/my-page-my-challenge.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DEFAULT_IMAGE from '/public/images/default_profile.png';
 import { Button } from '@/components/ui/button';
 import MyPageMyCompletedChallenges from './my-page-my-completed-challenges';
 import MyPageMyInProgressChallenges from './my-page-my-in-progress-challenges';
 import MyPageMyTodaysCompletedChallenges from './my-page-my-challenges-completions-today';
+import { getUserInfo } from '@/lib/api/user-Info.api';
 
 type MyChallengeAlias = 'Today' | '참여중' | '완료';
 type MyPageMyChallenges = {
@@ -16,6 +17,9 @@ type MyPageMyChallenges = {
 
 const MyPageMyChallenge = () => {
   const [selectedChallenge, setSelectedChallenge] = useState<MyChallengeAlias>('Today');
+  const [isLoading, setIsLoading] = useState(false);
+  const [nickname, setNickname] = useState('');
+  const [profileImg, setProfileImg] = useState('');
   const challenges: MyPageMyChallenges[] = [
     { label: '오늘 인증한 챌린지', alias: 'Today' },
     { label: '참여 중인 챌린지', alias: '참여중' },
@@ -36,20 +40,34 @@ const MyPageMyChallenge = () => {
     }
   };
   const { title, Component } = challengeComponents[selectedChallenge];
+
+  useEffect(() => {
+    setIsLoading(true);
+    getUserInfo()
+      .then((data) => {
+        const { nickname, profile_image } = data.userInfo;
+        setNickname(nickname);
+        setProfileImg(profile_image || '');
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) return;
+
   return (
     <section className="mt-5 flex w-full flex-col items-center gap-3 sm:flex-row">
       {/* 프로필 & 버튼 */}
       <aside className="flex w-full flex-col items-center sm:w-[30%]">
         <div className="hidden w-60 flex-col gap-5 rounded-2xl border-1 border-black p-10 text-center sm:block">
           <Image
-            src={DEFAULT_IMAGE}
+            src={profileImg || DEFAULT_IMAGE}
             alt="profile"
             width={200}
             height={200}
-            className="mt-3 rounded-full sm:mt-5"
+            className="mt-3 mb-3 rounded-full sm:mt-5"
             priority
           />
-          <p className="font-bold">Nickname</p>
+          <p className="font-bold">{nickname}</p>
         </div>
         <nav className="flex gap-2 sm:mt-5 sm:flex-col">
           {challenges.map((challenge) => (
