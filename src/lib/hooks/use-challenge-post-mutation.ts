c156a549294cda7e844a8375fa35351b404c9581
verdantgreeny <verdantgreeny@gmail.com>
2025-03-27 01:34:07 +0900

    fix: #52 useChallengePostMutation 수정

--- src/lib/hooks/use-challenge-post-mutation.ts
+++ src/lib/hooks/use-challenge-post-mutation.ts
@@ -1,14 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 import { ChallengePost } from '@/types/challenge.type';
 import { fetchCreateChallenge, fetchUpdateChallenge } from '../api/challenge-post.api';
-import { useRouter } from 'next/navigation';
 
-interface UseChallengePostMutationParams {
+type UseChallengePostMutationParams = {
   isEditMode: boolean;
   challenge: ChallengePost;
   challengeImageFile: File | null;
   challengeId?: number;
-}
+};
 
 const useChallengePostMutation = ({
   isEditMode,
@@ -16,31 +15,21 @@ const useChallengePostMutation = ({
   challengeImageFile,
   challengeId
 }: UseChallengePostMutationParams) => {
-  const router = useRouter();
-
-  const { mutate, isPending } = useMutation({
+  return useMutation<
+    {
+      success: boolean;
+      message: string;
+    },
+    Error
+  >({
     mutationFn: async () => {
       if (isEditMode) {
         if (challengeId === undefined) throw new Error('챌린지 ID가 없습니다.');
         return fetchUpdateChallenge(challengeId, challenge, challengeImageFile);
-      } else {
-        return fetchCreateChallenge(challenge, challengeImageFile);
       }
-    },
-    onSuccess: (data) => {
-      if (data.success) {
-        alert(data.message);
-        router.push(isEditMode ? `/challenges/${challengeId}` : '/home');
-      } else {
-        alert(`${data.message}`);
-      }
-    },
-    onError: (error) => {
-      alert(`${error instanceof Error ? error.message : '알 수 없는 오류'}`);
+      return fetchCreateChallenge(challenge, challengeImageFile);
     }
   });
-
-  return { mutate, isPending }; // mutate로 반환
 };
 
 export default useChallengePostMutation;
