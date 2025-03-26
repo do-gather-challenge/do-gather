810179cae014914555eb044eeb738bdc0b8e34fc
woohyuckk <woohyuckk@gmail.com>
2025-03-26 18:56:41 +0900

    add:#44 my-page 경로 추가

--- src/constants/app-url.constant.ts
+++ src/constants/app-url.constant.ts
@@ -3,9 +3,10 @@ const URL = {
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
   CHALLENGES: '/challenges',
-  CHALLENGES_ID: (challengeId:number) => `/challenges/${challengeId}`,
+  CHALLENGES_ID: (challengeId: number) => `/challenges/${challengeId}`,
   CHALLENGES_POST: '/challenges/post',
   CHALLENGES_POST_ID: (challengeId: number) => `/challenges/post/${challengeId}`,
-}
+  MY_PAGE: '/my-page'
+};
 
-export default URL 
+export default URL;
