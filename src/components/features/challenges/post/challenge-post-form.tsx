2c46bcbaf4b577a4fd074863734cd8b9e790c084
verdantgreeny <verdantgreeny@gmail.com>
2025-03-26 23:27:17 +0900

    design: #52 챌린지 제목 스타일 변경

--- src/components/features/challenges/post/challenge-post-form.tsx
+++ src/components/features/challenges/post/challenge-post-form.tsx
@@ -16,25 +16,32 @@ const ChallengePostForm = ({ mode, initialData }: ChallengeFormProps) => {
   const { challenge, challengeImageFile, setters, handleChange } = useChallengeForm(initialData);
 
   return (
-    <section className="mx-auto mb-6 max-w-[320px] p-6 md:max-w-[640px]">
-      <h1 className="mb-6 text-2xl font-bold">{mode === 'create' ? '챌린지 생성' : '챌린지 수정'}</h1>
-
-      <ChallengePostInput challenge={challenge} handleChange={handleChange} />
-
-      <section className="mb-6 grid grid-cols-1 gap-6 md:grid-cols-2">
-        <ChallengePostSelector challenge={challenge} setters={setters} />
-        <ChallengePostImageUploader setters={setters} challenge={challenge} />
-      </section>
-
-      <div className="flex justify-center gap-6">
-        <ChallengePostButtonGroup
-          challenge={challenge}
-          challengeImageFile={challengeImageFile}
-          isEditMode={mode === 'edit'}
-          challengeId={initialData?.id}
-        />
+    <>
+      <div className="relative mx-auto mt-24 w-full max-w-[1200px] px-5 sm:px-6 md:px-8">
+        <div className="bg-gray absolute right-10 bottom-0 left-10 h-px"></div>
+        <h1 className="relative mx-auto ml-15 inline-block border-b-3 border-black pb-1 text-2xl font-extrabold md:ml-25">
+          {mode === 'create' ? '챌린지 생성' : '챌린지 수정'}
+        </h1>
       </div>
-    </section>
+
+      <section className="mx-auto mb-6 max-w-[320px] p-6 md:max-w-[640px]">
+        <ChallengePostInput challenge={challenge} handleChange={handleChange} />
+
+        <section className="mb-6 grid grid-cols-1 gap-6 md:grid-cols-2">
+          <ChallengePostSelector challenge={challenge} setters={setters} />
+          <ChallengePostImageUploader setters={setters} challenge={challenge} />
+        </section>
+
+        <div className="flex justify-center gap-6">
+          <ChallengePostButtonGroup
+            challenge={challenge}
+            challengeImageFile={challengeImageFile}
+            isEditMode={mode === 'edit'}
+            challengeId={initialData?.id}
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
