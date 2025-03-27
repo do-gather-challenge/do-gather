6ac45140a2ec7d4514cf177803c52af15dd1533f
woohyuckk <woohyuckk@gmail.com>
2025-03-27 10:22:12 +0900

    refact:#74 post 페이지 toast 적용

--- src/components/features/challenges/post/challenge-post-button-group.tsx
+++ src/components/features/challenges/post/challenge-post-button-group.tsx
@@ -4,6 +4,7 @@ import URL from '@/constants/app-url.constant';
 import useChallengePostMutation from '@/lib/hooks/use-challenge-post-mutation';
 import { ChallengePost } from '@/types/challenge.type';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 type ChallengePostButtonGroupProps = {
   challenge: ChallengePost;
@@ -30,14 +31,14 @@ const ChallengePostButtonGroup = ({
     try {
       const result = await mutateAsync();
       if (result.success) {
-        alert(isEditMode ? '챌린지 수정 완료!' : '챌린지 생성 완료!');
+        toast.success(isEditMode ? '챌린지 수정 완료!' : '챌린지 생성 완료!');
         router.push(isEditMode ? APP_URL.CHALLENGES_ID(challengeId!) : APP_URL.HOME);
       } else {
-        alert(`오류: ${result.message}`);
+        toast.error(`오류: ${result.message}`);
       }
     } catch (error) {
       const message = error instanceof Error ? error.message : '알 수 없는 오류';
-      alert(`오류 발생: ${message}`);
+      toast.error(`오류 발생: ${message}`);
     }
   };
 
