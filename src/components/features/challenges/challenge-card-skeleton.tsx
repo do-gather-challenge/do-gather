860a0bd04ee80fe1d2b2109ec5683aff68d49ce8
hoki9797 <hoki9797@gmail.com>
2025-03-27 09:33:19 +0900

    chore:#60 rem 명시에서 기본 w-number로 변경

--- src/components/features/challenges/challenge-card-skeleton.tsx
+++ src/components/features/challenges/challenge-card-skeleton.tsx
@@ -2,11 +2,11 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 const ChallengeCardSkeleton = () => {
   return (
-    <div className="flex flex-col h-[16rem] space-y-6">
-      <Skeleton className="h-[8rem] w-[15rem] rounded-lg" />
+    <div className="flex flex-col h-64 space-y-6">
+      <Skeleton className="h-32 w-60 rounded-lg" />
       <div className="space-y-4">
-        <Skeleton className="h-4 w-[15rem]" />
-        <Skeleton className="h-4 w-[13rem]" />
+        <Skeleton className="h-4 w-60" />
+        <Skeleton className="h-4 w-52" />
       </div>
     </div>
   );
